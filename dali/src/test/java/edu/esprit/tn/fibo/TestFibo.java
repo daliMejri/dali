b918ae7837a8ba26521f34d2b44500fdd6a1de30
dali <dali@mouhamed.esprit.tn>
2014-05-28 10:20:59 +0200

    ssss
    Signed-off-by: dali <dali@mouhamed.esprit.tn>

---
+++ dali/src/test/java/edu/esprit/tn/fibo/TestFibo.java
@@ -0,0 +1,29 @@
+package edu.esprit.tn.fibo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestFibo {
+
+	private Fibonacci fibonacci;
+
+	@Before
+	public void setUp() throws Exception {
+		
+		fibonacci = new Fibonacci();
+	}
+
+	@Test
+	public void itShouldReturn1when2entred() {
+		assertEquals(1, fibonacci.process(2));
+	}
+	
+	
+	@Test
+	public void itShouldReturn2when3entred() {
+		assertEquals(2, fibonacci.process(3));
+	}
+
+}
